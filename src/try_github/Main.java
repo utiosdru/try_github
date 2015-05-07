ffeb92417ea066f09f5f2c281ffd1c668c194447
User <User@H27-NotePC01>
2015-05-07 15:59:21 +0900

    標準出力へ文字を表示

--- src/try_github/Main.java
+++ src/try_github/Main.java
@@ -3,7 +3,7 @@ package try_github;
 public class Main {
 
 	public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println("Hello Git with Eclipse");
 
     }
 
